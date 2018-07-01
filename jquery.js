60a4d1ce872822303a43ea64ea957fce8046d0f7
paryavarantiwari <39253207+paryavarantiwari@users.noreply.github.com>
2018-07-01 18:44:01 -0400

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("main section p").hide();
+    $("main section a").hover(function(){
+        $(this).css("text-decoration","underline");
+    },function(){
+        $(this).css("text-decoration","none");
+    }).click(function(a){
+        a.preventDefault();
+        $("main section p").hide();
+        
+        $(this).next('p').toggle();
+        
+    });
+    
+    
+});
